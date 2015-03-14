41093dae884d90b1331f5da3eb00da3457470485
Luciano Andrade <andrade.luciano@gmail.com>
2015-03-14 09:31:48 -0300

    Added wellcom, player one and player two pages.

--- web/index.php
+++ web/index.php
@@ -3,8 +3,26 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application(); 
 
-$app->get('/hello/{name}', function($name) use($app) { 
-    return 'Hello '.$app->escape($name); 
+$app->get('/', function() use($app) { 
+    return 'Wellcome page';
 }); 
+
+$app->get('/games/{user}', function($user) use($app) { 
+    if(!in_array($user, array('1','2'))){
+	throw new \Exception;
+    }
+    return 'Wellcome  User '.$user;
+}); 
+
+$app->error(function (\Exception $e, $code) use ($app) {
+
+    if ($code == 404) {
+
+        return "Page not found";
+    }
+
+    return 'We are sorry, but something went terribly wrong - code: '. $code;
+
+});
 
 $app->run(); 
